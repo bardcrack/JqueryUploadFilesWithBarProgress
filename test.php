946d5b1b72e6560ef5999a2903b6e186eab64b4a
Julio Bardcrack <accesoriosescalante@gmail.com>
2016-09-06 11:12:17 -0600

    Update test.php
    La funcion se llama token_generator, y tenia el nombre de token en la linea 5.
    Razón por la cual daba error al enviar el archivo.

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require_once("token.php");
 session_start();
 error_reporting(0);
-$token		=	token(20);//Funcion que me genera un valor de 20 caracteres en forma aleatoria
+$token		=	token_generator(20);//Funcion que me genera un valor de 20 caracteres en forma aleatoria
 echo "
 <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
 <html xmlns='http://www.w3.org/1999/xhtml'>
